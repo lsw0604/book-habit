d821b49569fba0c115d90bbfd420551dd0f584fb
lsw0604 <57swji@gmail.com>
2023-07-23 20:58:45 +0900

    eslint react-query dev-tools

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { RecoilRoot } from 'recoil';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RecoilRoot>
-      <App />
-    </RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <App />
+      </RecoilRoot>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </React.StrictMode>
 );
