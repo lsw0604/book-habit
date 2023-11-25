03e9acd083de2cd51bcc09bd5925b60fd84b5bb2
lsw0604 <57swji@gmail.com>
2023-11-25 23:24:03 +0900

    Modify profile -1

--- Frontend/src/components/Modals/Profile/ProfileModify.tsx
+++ Frontend/src/components/Modals/Profile/ProfileModify.tsx
@@ -1,11 +1,9 @@
 import Button from 'components/common/Button';
 import CheckBoxGroup from 'components/common/CheckBox';
 import Input from 'components/common/Input';
-import { useRecoilState } from 'recoil';
-import { userAtom } from 'recoil/user';
 import styled from 'styled-components';
 import { CheckBoxOptionType, RadioGroupOptionType } from 'types/style';
-import { useState } from 'react';
+import { ChangeEvent, useCallback, useState } from 'react';
 import RadioGroup from 'components/common/Radio';
 import { IconFemale, IconMale } from '@style/icons';
 
@@ -32,6 +30,10 @@ const Header = styled.div`
 const Content = styled.div`
   position: relative;
   margin-bottom: 1rem;
+`;
+
+const Stack = styled.div`
+  margin-bottom: 8px;
 `;
 
 const Footer = styled.div`
@@ -69,52 +71,69 @@ const radioOptions: RadioGroupOptionType<'male' | 'female' | ''>[] = [
 ];
 
 export default function ProfileModify() {
-  const [userState, setUserState] = useRecoilState(userAtom);
   const [value, setValue] = useState<CheckBoxOptionType<string>[]>([]);
+
+  const [gender, setGender] = useState<GenderType | undefined>('');
+  const [age, setAge] = useState<'' | number>('');
+  const [name, setName] = useState<string>('');
 
   const onChange = (selected: CheckBoxOptionType<string>[]) => {
     setValue(selected);
   };
 
+  const onChangeName = useCallback(
+    (event: ChangeEvent<HTMLInputElement>) => {
+      setName(event.target.value);
+    },
+    [name, setName]
+  );
+
+  const onChangeGender = (ctx?: GenderType) => {
+    setGender(ctx);
+  };
+
+  const onChangeAge = useCallback(
+    (event: ChangeEvent<HTMLInputElement>) => {
+      const value = event.target.value;
+      const isValid = /^\d+$/.test(value);
+
+      if (!isValid) return setAge('');
+
+      setAge(parseInt(value, 10));
+    },
+    [age, setAge]
+  );
+
   return (
     <Container>
       <Header>프로필 수정하시겠습니까?</Header>
-      <CheckBoxGroup<string>
-        options={checkboxOptions}
-        onChange={onChange}
-        value={value}
-      />
+      <Stack>
+        <CheckBoxGroup<string>
+          options={checkboxOptions}
+          onChange={onChange}
+          value={value}
+        />
+      </Stack>
       <Content>
         {value.some((el) => el.title === '이름') && (
-          <Input
-            label="이름"
-            value={userState.name}
-            onChange={(e) =>
-              setUserState((prev) => ({ ...prev, name: e.target.value }))
-            }
-          />
+          <Stack>
+            <Input label="이름" value={name} onChange={onChangeName} />
+          </Stack>
         )}
         {value.some((el) => el.title === '나이') && (
-          <Input
-            label="나이"
-            value={userState.age}
-            onChange={(e) =>
-              setUserState((prev) => ({
-                ...prev,
-                age: parseInt(e.target.value),
-              }))
-            }
-          />
+          <Stack>
+            <Input label="나이" value={age} onChange={onChangeAge} />
+          </Stack>
         )}
         {value.some((el) => el.title === '성별') && (
-          <RadioGroup
-            onChange={(e) =>
-              setUserState((prev) => ({ ...prev, gender: e.target.value }))
-            }
-            options={radioOptions}
-            label="성별"
-            value={userState.gender as GenderType}
-          />
+          <Stack>
+            <RadioGroup<string>
+              onChange={() => onChangeGender()}
+              options={radioOptions}
+              label="성별"
+              value={gender as string}
+            />
+          </Stack>
         )}
       </Content>
       <Footer>
