2c46f1e388caded12428f28139ec54fa3b63e3c7
lsw0604 <57swji@gmail.com>
2024-07-02 14:43:36 +0900

    Add @nestjs/config & env

--- nest-server/src/app.module.ts
+++ nest-server/src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { BookModule } from './book/book.module';
 
 @Module({
-  imports: [PrismaModule, BookModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    PrismaModule,
+    BookModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
