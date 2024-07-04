7ca4f803f573ed3d56f0123768753dc6aae8dbe2
lsw0604 <57swji@gmail.com>
2024-07-04 19:47:34 +0900

    Update book service - 1

--- nest-server/src/book/book.service.ts
+++ nest-server/src/book/book.service.ts
@@ -9,11 +9,71 @@ export class BookService {
 
   async registerBook(
     dto: Omit<BookRegisterDto, 'isbn' | 'author' | 'translator'>,
-  ): Promise<Book> {
-    return this.prismaService.book.create({
-      data: {
-        ...dto,
-      },
-    });
+  ): Promise<number> {
+    try {
+      const { id } = await this.prismaService.book.create({
+        data: {
+          ...dto,
+        },
+      });
+
+      return id;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  registerIsbn(dto: Pick<BookRegisterDto, 'isbn'>, bookId: number) {
+    if (dto.isbn.length === 0) return;
+
+    try {
+      dto.isbn.forEach(async (isbn) => {
+        await this.prismaService.iSBN.create({
+          data: {
+            isbn,
+            bookId,
+          },
+        });
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  registerAuthor(dto: Pick<BookRegisterDto, 'authors'>, bookId: number) {
+    if (dto.authors.length === 0) return;
+
+    try {
+      dto.authors.forEach(async (name) => {
+        await this.prismaService.author.create({
+          data: {
+            name,
+            bookId,
+          },
+        });
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  registerTranslator(
+    dto: Pick<BookRegisterDto, 'translators'>,
+    bookId: number,
+  ) {
+    if (dto.translators.length === 0) return;
+
+    try {
+      dto.translators.forEach(async (translator) => {
+        await this.prismaService.translator.create({
+          data: {
+            translator,
+            bookId,
+          },
+        });
+      });
+    } catch (err) {
+      console.log(err);
+    }
   }
 }
