2729ab0d1f5ed8bc4e53d9011374774cfcd017c0
lsw0604 <57swji@gmail.com>
2024-07-05 23:48:32 +0900

    Modify Prisma Schema

--- nest-server/src/book/book.service.ts
+++ nest-server/src/book/book.service.ts
@@ -10,51 +10,47 @@ export class BookService {
   async registerBook(
     dto: Omit<BookRegisterDto, 'isbn' | 'author' | 'translator'>,
   ): Promise<number> {
-    try {
-      const { id } = await this.prismaService.book.create({
-        data: {
-          ...dto,
-        },
-      });
+    const { id } = await this.prismaService.book.create({
+      data: {
+        ...dto,
+      },
+    });
 
-      return id;
-    } catch (err) {
-      console.log(err);
-    }
+    return id;
+  }
+
+  async existIsbn(isbn: string) {
+    const { book } = this.prismaService.iSBN.findUnique({
+      where: {
+        isbn,
+      },
+    });
   }
 
   registerIsbn(dto: Pick<BookRegisterDto, 'isbn'>, bookId: number) {
     if (dto.isbn.length === 0) return;
 
-    try {
-      dto.isbn.forEach(async (isbn) => {
-        await this.prismaService.iSBN.create({
-          data: {
-            isbn,
-            bookId,
-          },
-        });
+    dto.isbn.forEach(async (isbn) => {
+      await this.prismaService.iSBN.create({
+        data: {
+          isbn,
+          bookId,
+        },
       });
-    } catch (err) {
-      console.log(err);
-    }
+    });
   }
 
   registerAuthor(dto: Pick<BookRegisterDto, 'authors'>, bookId: number) {
     if (dto.authors.length === 0) return;
 
-    try {
-      dto.authors.forEach(async (name) => {
-        await this.prismaService.author.create({
-          data: {
-            name,
-            bookId,
-          },
-        });
+    dto.authors.forEach(async (name) => {
+      await this.prismaService.author.create({
+        data: {
+          name,
+          bookId,
+        },
       });
-    } catch (err) {
-      console.log(err);
-    }
+    });
   }
 
   registerTranslator(
@@ -63,17 +59,13 @@ export class BookService {
   ) {
     if (dto.translators.length === 0) return;
 
-    try {
-      dto.translators.forEach(async (translator) => {
-        await this.prismaService.translator.create({
-          data: {
-            translator,
-            bookId,
-          },
-        });
+    dto.translators.forEach(async (translator) => {
+      await this.prismaService.translator.create({
+        data: {
+          translator,
+          bookId,
+        },
       });
-    } catch (err) {
-      console.log(err);
-    }
+    });
   }
 }
