6acfdca9a4065817939fcac1474e9bf3d2858a20
lsw0604 <57swji@gmail.com>
2025-03-26 22:47:48 +0900

    [수정] Prisma Version & Schema

--- nest-server/prisma/migrations/20240706120003_add_long_text_for_contents/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `book` MODIFY `contents` TEXT NULL;
--- nest-server/prisma/migrations/20250326064512_init_prisma/migration.sql
+++ nest-server/prisma/migrations/20250326064512_init_prisma/migration.sql
@@ -1,12 +1,14 @@
 -- CreateTable
 CREATE TABLE `Book` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `title` VARCHAR(191) NULL,
+    `title` VARCHAR(191) NOT NULL,
     `publisher` VARCHAR(191) NULL,
     `price` INTEGER NULL,
+    `sale_price` INTEGER NULL,
     `thumbnail` VARCHAR(191) NULL,
-    `contents` VARCHAR(191) NULL,
-    `url` VARCHAR(191) NULL,
+    `contents` TEXT NULL,
+    `url` TEXT NULL,
+    `datetime` DATETIME NOT NULL,
     `status` VARCHAR(191) NULL,
 
     PRIMARY KEY (`id`)
@@ -15,10 +17,10 @@ CREATE TABLE `Book` (
 -- CreateTable
 CREATE TABLE `ISBN` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `isbn` VARCHAR(191) NOT NULL,
+    `code` VARCHAR(191) NOT NULL,
     `bookId` INTEGER NOT NULL,
 
-    UNIQUE INDEX `ISBN_isbn_key`(`isbn`),
+    UNIQUE INDEX `ISBN_code_key`(`code`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -26,9 +28,32 @@ CREATE TABLE `ISBN` (
 CREATE TABLE `Author` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(191) NOT NULL,
-    `bookId` INTEGER NOT NULL,
 
     PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Translator` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `BookAuthor` (
+    `bookId` INTEGER NOT NULL,
+    `authorId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`bookId`, `authorId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `BookTranslator` (
+    `bookId` INTEGER NOT NULL,
+    `translatorId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`bookId`, `translatorId`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
@@ -38,9 +63,9 @@ CREATE TABLE `User` (
     `password` VARCHAR(191) NULL,
     `name` VARCHAR(191) NULL,
     `birthday` DATETIME(3) NULL,
-    `gender` ENUM('MALE', 'FEMALE') NULL,
-    `provider` ENUM('LOCAL', 'KAKAO') NULL,
-    `profile` VARCHAR(191) NULL DEFAULT 'https://pic.lsw0604.store/20231123_1205SS_e16f2d7c-ae50-4558-9b5a-e44f9be0bf2e.jpg',
+    `gender` ENUM('MALE', 'FEMALE') NOT NULL DEFAULT 'MALE',
+    `provider` ENUM('LOCAL', 'KAKAO') NOT NULL DEFAULT 'LOCAL',
+    `profile` VARCHAR(191) NULL,
 
     UNIQUE INDEX `User_email_key`(`email`),
     PRIMARY KEY (`id`)
@@ -51,8 +76,8 @@ CREATE TABLE `MyBook` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `userId` INTEGER NOT NULL,
     `bookId` INTEGER NOT NULL,
-    `rating` INTEGER NULL,
-    `myBookStatus` ENUM('TO_READ', 'START_READ', 'READING', 'READ') NOT NULL,
+    `rating` INTEGER NOT NULL DEFAULT 0,
+    `status` ENUM('WANT_TO_READ', 'CURRENTLY_READING', 'READ') NOT NULL DEFAULT 'WANT_TO_READ',
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
@@ -60,11 +85,10 @@ CREATE TABLE `MyBook` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `MyBookComment` (
+CREATE TABLE `MyBookReview` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `myBookId` INTEGER NOT NULL,
-    `comment` VARCHAR(191) NOT NULL,
-    `status` VARCHAR(191) NOT NULL,
+    `review` VARCHAR(191) NOT NULL,
     `isPublic` BOOLEAN NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
@@ -73,19 +97,19 @@ CREATE TABLE `MyBookComment` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `CommentLike` (
+CREATE TABLE `ReviewLike` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `userId` INTEGER NOT NULL,
-    `myBookCommentId` INTEGER NOT NULL,
+    `myBookReviewId` INTEGER NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `CommentReply` (
+CREATE TABLE `ReviewComment` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `reply` VARCHAR(191) NOT NULL,
-    `myBookCommentId` INTEGER NOT NULL,
+    `comment` VARCHAR(191) NOT NULL,
+    `myBookReviewId` INTEGER NOT NULL,
     `userId` INTEGER NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
@@ -96,8 +120,9 @@ CREATE TABLE `CommentReply` (
 -- CreateTable
 CREATE TABLE `Tag` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `tag` VARCHAR(191) NULL,
+    `value` VARCHAR(191) NOT NULL,
 
+    UNIQUE INDEX `Tag_value_key`(`value`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -113,9 +138,12 @@ CREATE TABLE `MyBookTag` (
 -- CreateTable
 CREATE TABLE `MyBookHistory` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `page` INTEGER NULL,
-    `date` DATETIME(3) NULL,
     `myBookId` INTEGER NOT NULL,
+    `startPage` INTEGER NULL,
+    `endPage` INTEGER NULL,
+    `date` DATETIME(3) NOT NULL,
+    `memo` VARCHAR(191) NULL,
+    `readingMood` ENUM('INSPIRED', 'EXCITED', 'INTRIGUED', 'SATISFIED', 'NEUTRAL', 'CONFUSED', 'DISAPPOINTED', 'BORED', 'EMOTIONAL', 'THOUGHTFUL', 'CHALLENGED', 'ENLIGHTENED') NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
@@ -126,7 +154,16 @@ CREATE TABLE `MyBookHistory` (
 ALTER TABLE `ISBN` ADD CONSTRAINT `ISBN_bookId_fkey` FOREIGN KEY (`bookId`) REFERENCES `Book`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `Author` ADD CONSTRAINT `Author_bookId_fkey` FOREIGN KEY (`bookId`) REFERENCES `Book`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `BookAuthor` ADD CONSTRAINT `BookAuthor_bookId_fkey` FOREIGN KEY (`bookId`) REFERENCES `Book`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `BookAuthor` ADD CONSTRAINT `BookAuthor_authorId_fkey` FOREIGN KEY (`authorId`) REFERENCES `Author`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `BookTranslator` ADD CONSTRAINT `BookTranslator_bookId_fkey` FOREIGN KEY (`bookId`) REFERENCES `Book`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `BookTranslator` ADD CONSTRAINT `BookTranslator_translatorId_fkey` FOREIGN KEY (`translatorId`) REFERENCES `Translator`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `MyBook` ADD CONSTRAINT `MyBook_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -135,19 +172,19 @@ ALTER TABLE `MyBook` ADD CONSTRAINT `MyBook_userId_fkey` FOREIGN KEY (`userId`) 
 ALTER TABLE `MyBook` ADD CONSTRAINT `MyBook_bookId_fkey` FOREIGN KEY (`bookId`) REFERENCES `Book`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `MyBookComment` ADD CONSTRAINT `MyBookComment_myBookId_fkey` FOREIGN KEY (`myBookId`) REFERENCES `MyBook`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `MyBookReview` ADD CONSTRAINT `MyBookReview_myBookId_fkey` FOREIGN KEY (`myBookId`) REFERENCES `MyBook`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `CommentLike` ADD CONSTRAINT `CommentLike_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `ReviewLike` ADD CONSTRAINT `ReviewLike_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `CommentLike` ADD CONSTRAINT `CommentLike_myBookCommentId_fkey` FOREIGN KEY (`myBookCommentId`) REFERENCES `MyBookComment`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `ReviewLike` ADD CONSTRAINT `ReviewLike_myBookReviewId_fkey` FOREIGN KEY (`myBookReviewId`) REFERENCES `MyBookReview`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `CommentReply` ADD CONSTRAINT `CommentReply_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `ReviewComment` ADD CONSTRAINT `ReviewComment_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `CommentReply` ADD CONSTRAINT `CommentReply_myBookCommentId_fkey` FOREIGN KEY (`myBookCommentId`) REFERENCES `MyBookComment`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `ReviewComment` ADD CONSTRAINT `ReviewComment_myBookReviewId_fkey` FOREIGN KEY (`myBookReviewId`) REFERENCES `MyBookReview`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `MyBookTag` ADD CONSTRAINT `MyBookTag_myBookId_fkey` FOREIGN KEY (`myBookId`) REFERENCES `MyBook`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
