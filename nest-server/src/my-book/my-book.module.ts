f3c162ae97e0596d514960b1217be9ff29310550
lsw0604 <57swji@gmail.com>
2025-04-25 13:42:26 +0900

    🪵 Fix: Book 및 MyBook 모듈에 LoggerService 추가
    BookModule과 MyBookModule의 providers 배열에 누락되었던 LoggerService를 추가합니다.
    이를 통해 해당 모듈 내에서 LoggerService를 주입받아 로깅 기능을 정상적으로 사용할 수 있습니다.

--- nest-server/src/my-book/my-book.module.ts
+++ nest-server/src/my-book/my-book.module.ts
@@ -3,10 +3,11 @@ import { MyBookController } from './my-book.controller';
 import { MyBookService } from './my-book.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { BookModule } from 'src/book/book.module';
+import { LoggerService } from 'src/common/logger/logger.service';
 
 @Module({
   imports: [BookModule],
-  providers: [MyBookService, PrismaService],
+  providers: [MyBookService, PrismaService, LoggerService],
   controllers: [MyBookController],
   exports: [MyBookService],
 })
