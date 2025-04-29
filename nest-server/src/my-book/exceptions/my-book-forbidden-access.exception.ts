3333b241ecc70de78bd25f91338c78ed55f85f98
lsw0604 <57swji@gmail.com>
2025-04-29 15:31:15 +0900

    📝 refactor(my-book): 예외 메세지 수정

--- nest-server/src/my-book/exceptions/my-book-forbidden-access.exception.ts
+++ nest-server/src/my-book/exceptions/my-book-forbidden-access.exception.ts
@@ -3,12 +3,12 @@ import { ForbiddenException } from '@nestjs/common';
 export class MyBookForbiddenAccessException extends ForbiddenException {
   constructor(params: { myBookId: number; ownerId: number; userId: number }) {
     const { myBookId, ownerId, userId } = params;
-    const message = `User ID : ${userId}는 MyBook(ID : ${myBookId})에 접근할 권한이 없습니다. (소유자 ID : ${ownerId})`;
+    const message = `USER (ID: ${userId})는 MY BOOK(ID: ${myBookId})에 접근할 권한이 없습니다. 소유자(ID: ${ownerId})`;
     const errorMetadata = {
       myBookId,
       ownerId,
       userId,
-      errorCode: 'MY_BOOK_ACCESS_FORBIDDEN',
+      errorCode: 'MY_BOOK_FORBIDDEN_ACCESS',
     };
 
     super({
