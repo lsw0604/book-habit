957c600d84b8ca02eff3570b83c2c81bd8def9a0
이성원 <57swji@gmail.com>
2025-12-07 14:31:36 +0900

    ✨featswagger): swagger 추가

--- nest-server/src/main.ts
+++ nest-server/src/main.ts
@@ -5,6 +5,7 @@ import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 import { ResponseDtoInterceptor } from './common/interceptors';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function setUpMiddleware(app: INestApplication) {
   app.use(cookieParser());
@@ -17,12 +18,27 @@ async function setUpInterceptor(app: INestApplication) {
   app.useGlobalInterceptors(new ResponseDtoInterceptor());
 }
 
+function setUpSwagger(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('API Document') // 문서 제목
+    .setDescription('API 설명') // 문서 설명
+    .setVersion('1.0') // 버전
+    .addBearerAuth() // JWT 토큰 인증이 필요하다면 추가
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  // http://localhost:3000/api 로 접속 가능하게 설정
+  SwaggerModule.setup('api', app, document);
+}
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const allowOrigins = process.env.CORS_ORIGINS.split(',');
 
   await setUpMiddleware(app);
   await setUpInterceptor(app);
+  setUpSwagger(app);
 
   app.enableCors({
     origin: (origin, callback) => {
